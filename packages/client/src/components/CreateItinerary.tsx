0eb1eee9df198c5fa9eb075c050fdcab77d3eaee
George Botros <78520093+george-botros@users.noreply.github.com>
2023-11-28 19:38:19 -0500

    feat: separate itinerary create form into sections with input types

--- packages/client/src/components/CreateItinerary.tsx
+++ packages/client/src/components/CreateItinerary.tsx
@@ -1,54 +1,83 @@
-import { Box, TextField, Button, Typography, Paper } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'; // Icon for Dropdown
+import DateRangeIcon from '@mui/icons-material/DateRange'; // Icon for Dates
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar'; // Icon for Travel
+import EventIcon from '@mui/icons-material/Event'; // Icon for Events
+import HotelIcon from '@mui/icons-material/Hotel'; // Icon for Lodging
+import LocationCityIcon from '@mui/icons-material/LocationCity'; // Icon for Location
+import { Box, TextField, Button, Typography, Paper, Select, MenuItem, FormControl, InputLabel, Divider } from '@mui/material';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 type Activity = {
   id: number;
-  type: string; // such as 'Hotel', 'Hike', 'Museum', etc.
-  name: string; // could be the hotel name, hiking trail, museum name, etc.
-  date: string; // when they plan to go
-  details: string; // room number for hotel, guide name for hike, etc.
-  address: string; // common field for all activities
+  type: string;
+  name: string;
+  date: string;
+  details: string;
+  address: string;
 };
 
 export default function CreateItinerary() {
   const navigate = useNavigate();
+  const [activities, setActivities] = useState([{ id: 0, type: '', name: '', date: '', details: '', address: '' }]);
+  const [itinerary, setItinerary] = useState({
+    tripName: '',
+    location: '',
+    participants: '',
+    description: '',
+    startDate: '',
+    endDate: '',
+    travelInfoBefore: {
+      travelType: '',
+      travelTime: '',
+      details: '',
+    },
+    travelInfoAfter: {
+      travelType: '',
+      travelTime: '',
+      details: '',
+    },
+    lodgingName: '',
+    lodgingAddress: '',
+  });
 
-  const [activities, setActivities] = useState<Activity[]>([]); // Array of activities for trip.
+  const TRAVEL_TYPES = ['Airplane', 'Train', 'Bus', 'Car', 'Other'];
+  const ACTIVITY_TYPES = [
+    'Sightseeing Tour',
+    'Outdoor Adventure',
+    'Cultural Experience',
+    'Food and Drink',
+    'Recreational Sport',
+    'Nature Exploration',
+    'Leisure and Relaxation',
+    'Educational Activities',
+    'Nightlife',
+    'Art and Craft',
+    'Local Experiences',
+    'Fitness and Wellness',
+    'Historical Site',
+    'Volunteering',
+    'Other',
+  ];
 
   const addActivityField = () => {
-    // Use a unique id for key, for example, the next array length could be used
-    setActivities([
-      ...activities,
-      {
-        id: activities.length,
-        type: '',
-        name: '',
-        date: '',
-        details: '',
-        address: '',
-      },
-    ]);
+    setActivities([...activities, { id: activities.length, type: '', name: '', date: '', details: '', address: '' }]);
   };
 
-  // Function to update an activity
   const updateActivity = (index: number, field: keyof Activity, value: string) => {
     const newActivities = [...activities];
     (newActivities[index][field] as any) = value;
     setActivities(newActivities);
   };
 
-  // Function to handle form submission
+  const handleChange = (field: keyof typeof itinerary, value: any) => {
+    setItinerary({ ...itinerary, [field]: value });
+  };
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const itinerary = {
-      activities: activities,
-    };
-
     try {
-      // Send info to API later...
-      console.log('Itinerary to be sent: ', itinerary);
+      console.log('Itinerary to be sent: ', { ...itinerary, activities });
       navigate('/feed');
     } catch (error) {
       console.log('Error: ', error);
@@ -56,16 +85,105 @@ export default function CreateItinerary() {
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit} sx={{ '& .MuiTextField-root': { m: 1 } }}>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        '& .MuiTextField-root': { m: 1 },
+        'padding': '2rem', // This adds padding to the entire page
+        'maxWidth': '800px',
+        'margin': 'auto', // This centers the form on the page
+      }}
+    >
+      <Typography variant="h4" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+        <LocationCityIcon sx={{ mr: 1 }} /> Basic Information
+      </Typography>
+      <Paper sx={{ padding: '1rem', marginBottom: '1rem' }}>
+        <TextField label="Trip Name" value={itinerary.tripName} onChange={e => handleChange('tripName', e.target.value)} fullWidth />
+        <TextField label="Location (City, Country)" value={itinerary.location} onChange={e => handleChange('location', e.target.value)} fullWidth />
+        <TextField label="Number of Participants" type="number" value={itinerary.participants} onChange={e => handleChange('participants', e.target.value)} fullWidth />
+        <TextField label="Description" multiline rows={4} value={itinerary.description} onChange={e => handleChange('description', e.target.value)} fullWidth />
+      </Paper>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Paper sx={{ padding: '1rem', marginBottom: '1rem' }}>
+        <Typography variant="h4" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+          <DateRangeIcon sx={{ mr: 1 }} /> Dates
+        </Typography>
+        <TextField label="Start Date" type="date" InputLabelProps={{ shrink: true }} value={itinerary.startDate} onChange={e => handleChange('startDate', e.target.value)} fullWidth />
+        <TextField label="End Date" type="date" InputLabelProps={{ shrink: true }} value={itinerary.endDate} onChange={e => handleChange('endDate', e.target.value)} fullWidth />
+      </Paper>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Typography variant="h4" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+        <DirectionsCarIcon sx={{ mr: 1 }} /> Travel Information
+      </Typography>
+      <Paper sx={{ padding: '1rem', marginBottom: '1rem' }}>
+        <Typography variant="h6">Travel Information to Trip:</Typography>
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel>Travel Type</InputLabel>
+          <Select
+            value={itinerary.travelInfoBefore.travelType}
+            onChange={e => handleChange('travelInfoBefore', { ...itinerary.travelInfoBefore, travelType: e.target.value })}
+            label="Travel Type"
+            IconComponent={ArrowDropDownIcon}
+          >
+            {TRAVEL_TYPES.map((type, index) => (
+              <MenuItem key={index} value={type}>{type}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField label="Travel Time" type="time" InputLabelProps={{ shrink: true }} value={itinerary.travelInfoBefore.travelTime} onChange={e => handleChange('startDate', e.target.value)} fullWidth />
+
+        <Typography variant="h6">Travel Information after Trip:</Typography>
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel>Travel Type</InputLabel>
+          <Select
+            value={itinerary.travelInfoAfter.travelType}
+            onChange={e => handleChange('travelInfoAfter', { ...itinerary.travelInfoAfter, travelType: e.target.value })}
+            label="Travel Type"
+            IconComponent={ArrowDropDownIcon}
+          >
+            {TRAVEL_TYPES.map((type, index) => (
+              <MenuItem key={index} value={type}>{type}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField label="Travel Time" type="time" InputLabelProps={{ shrink: true }} value={itinerary.travelInfoAfter.travelTime} onChange={e => handleChange('startDate', e.target.value)} fullWidth />
+      </Paper>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Typography variant="h4" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+        <HotelIcon sx={{ mr: 1 }} /> Lodging
+      </Typography>
+      <Paper sx={{ padding: '1rem', marginBottom: '1rem' }}>
+        <TextField label="Lodging Location Name" value={itinerary.lodgingName} onChange={e => handleChange('lodgingName', e.target.value)} fullWidth />
+        <TextField label="Lodging Address" value={itinerary.lodgingAddress} onChange={e => handleChange('lodgingAddress', e.target.value)} fullWidth />
+      </Paper>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Typography variant="h4" gutterBottom sx={{ display: 'flex', alignItems: 'center' }}>
+        <EventIcon sx={{ mr: 1 }} /> Events Planned
+      </Typography>
       {activities.map((activity, index) => (
         <Paper key={activity.id} sx={{ padding: '1rem', marginBottom: '1rem' }}>
-          <Typography variant="h6" gutterBottom>Activity {index + 1}</Typography>
-          <TextField
-            label="Activity Type (e.g., Hotel, Hike, Museum)"
-            value={activity.type}
-            onChange={e => updateActivity(index, 'type', e.target.value)}
-            fullWidth
-          />
+          <Typography variant="h6">Activity {index + 1}</Typography>
+          <FormControl fullWidth sx={{ m: 1 }}>
+            <InputLabel>Activity Type</InputLabel>
+            <Select
+              value={activity.type}
+              onChange={e => updateActivity(index, 'type', e.target.value)}
+              label="Activity Type"
+            >
+              {ACTIVITY_TYPES.map((type, idx) => (
+                <MenuItem key={idx} value={type}>{type}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <TextField
             label="Activity Name"
             value={activity.name}
@@ -73,35 +191,33 @@ export default function CreateItinerary() {
             fullWidth
           />
           <TextField
-            label="Date"
+            label="Date & Time"
+            type="datetime-local"
+            InputLabelProps={{ shrink: true }}
             value={activity.date}
             onChange={e => updateActivity(index, 'date', e.target.value)}
             fullWidth
           />
           <TextField
-            label="Details"
-            value={activity.details}
-            onChange={e => updateActivity(index, 'details', e.target.value)}
-            fullWidth
-          />
-          <TextField
-            label="Address"
+            label="Location"
             value={activity.address}
             onChange={e => updateActivity(index, 'address', e.target.value)}
             fullWidth
           />
-          {/* Button to remove the activity field */}
-          <Button
-            variant="outlined"
-            color="error"
-            onClick={() => setActivities(activities.filter((_, i) => i !== index))}
-            sx={{ mt: 1 }}
-          >
+          <TextField
+            label="Description"
+            multiline
+            rows={3}
+            value={activity.details}
+            onChange={e => updateActivity(index, 'details', e.target.value)}
+            fullWidth
+          />
+          <Button variant="outlined" color="error" onClick={() => setActivities(activities.filter((_, i) => i !== index))} sx={{ mt: 1 }}>
             Remove Activity
           </Button>
         </Paper>
       ))}
-      <Button onClick={addActivityField} variant="outlined" sx={{ mt: 1 }}>Add Another Activity</Button>
+      <Button onClick={addActivityField} variant="outlined" sx={{ mt: 1, mr: 2 }}>Add Another Activity</Button>
       <Button type="submit" variant="contained" color="primary" sx={{ mt: 1 }}>Save Itinerary</Button>
     </Box>
   );
